0fe1c6bd48283f635ea8b98260b3074d1f065f2e
karticn-google <karticn@google.com>
2022-10-08 20:32:35 -0500

    updated cleanup.sh and README.md

--- cleanup.sh
+++ cleanup.sh
@@ -15,17 +15,78 @@
 # limitations under the License.
 ####################################################################################
 
-####################################################################################
-# README
-#
-# This script will delete all the Terraform lock and state files
-# This will reset Terraform and you will need to run a "source deploy.sh" again which will create a new project and deploy resources
-# GCP Project created in the previous run can be deleted using the command "gcloud projects delete PROJECT_ID_OR_NUMBER"
-# Author: Kartic Narayanan
-# Terraform for Google: https://registry.terraform.io/providers/hashicorp/google/latest/docs
-####################################################################################
 
+# Get the account name who logged in above 
+gcp_account_name=$(gcloud auth list --filter=status:ACTIVE --format="value(account)")
+
+
+# Get the Org Id (required for org policies and creating the GCP project)
+org_id=$(gcloud organizations list --format="value(name)")
+if [ -z "${org_id}" ]
+then
+  echo "Org Id could not be automatically read."
+  echo "Open this link: https://console.cloud.google.com/cloud-resource-manager/ and copy your org id."
+  echo "Your org id will be in the format of xxxxxxxxxxxx"
+  read -p "Please enter your org id:" org_id
+else
+  org_id_length=$(echo -n "${org_id}" | wc -m)
+  org_id_length_int=$(expr ${org_id_length} + 0)
+  if [ ${org_id_length_int} != 12 ]
+  then
+    echo "You have more than one org id, please manually enter the correct one."
+    echo "Your org id will be in the format of xxxxxxxxxxxx"
+    read -p "Please enter your org id:" org_id
+  else
+    echo "Org Id was automatically retreived."
+  fi
+fi
+
+# Get the Billing Account (required for creating the GCP project)
+billing_account=$(gcloud beta billing accounts list --format="value(ACCOUNT_ID)")
+if [ -z "${billing_account}" ]
+then
+  echo "Billing Account could not be automatically read."
+  echo "Open this link: https://console.cloud.google.com/billing/ and copy your billing account."
+  echo "Your billing account will be in the format of xxxxxx-xxxxxx-xxxxxx"
+  read -p "Please enter your billing account:" billing_account
+else
+  billing_account_length=$(echo -n "${billing_account}" | wc -m)
+  billing_account_length_int=$(expr ${billing_account_length} + 0)
+  if [ ${billing_account_length_int} != 20 ]
+  then
+    echo "You have more than one billing account, please manually enter the correct one."
+    echo "Your billing account will be in the format of xxxxxx-xxxxxx-xxxxxx"
+    read -p "Please enter your billing account:" billing_account
+  else
+    echo "Billing Account was automatically retreived."
+  fi
+fi
+
+
+
+echo "gcp_account_name: ${gcp_account_name}"
+echo "billing_account:  ${billing_account}"
+echo "org_id:           ${org_id}"
+
+
+echo "*********************************************************"
+read -p "Press [Enter] key if all the above items are set (gcp_account_name, org_id, billing_account). If not press Ctrl+C ..."
+echo "*********************************************************"
+
+cd "./terraform"
+
+echo "terraform destroy -var=\"org_id=${org_id}\" -var=\"billing_account=${billing_account}\" -auto-approve"
+
+##Run the Terraform Destroy to delete all the resources##
+terraform destroy \
+  -var="org_id=${org_id}" \
+  -var="billing_account=${billing_account}" \
+  -auto-approve
+
+##Remove Terraform state file, lock.hcl file and .terraform folder##
 rm -r ./terraform/.terraform
 rm ./terraform/.terraform.lock.hcl
 rm ./terraform/terraform.tfstate
 rm ./terraform/terraform.tfstate.backup
+
+echo "******Terraform Destroy Completed Successfully******"
